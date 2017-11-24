9da0f2c97e9f5a136d9220e3f0621c3ef892a7fb
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2017-11-23 16:08:18 -0800

    Prototype with bootstrap+blang/ace working

--- doc/download-deps.sh
+++ doc/download-deps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# ACE
 rm -rf ace-master
 git clone https://github.com/ajaxorg/ace.git
 mv ace ace-master
@@ -8,3 +9,11 @@ git reset --hard c3403f1fbdf22cfff2cb1dda584b8e04467cd372
 cd -
 
 
+# Bootstrap
+rm -rf bootstrap
+rm -rf www/dist
+git clone https://github.com/twbs/bootstrap.git
+cd bootstrap
+git reset --hard v3.3.7
+cp dist ../www/
+cd -
