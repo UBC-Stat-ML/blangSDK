6eb3839dc40a072dc74c707a5073c6c9ab010b22
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2018-01-21 15:46:35 -0800

    Fix bug preventing reading configuration.txt, add limits to Exact

--- src/main/java/blang/engines/internals/factories/Exact.java
+++ src/main/java/blang/engines/internals/factories/Exact.java
@@ -5,6 +5,8 @@ import org.eclipse.xtext.xbase.lib.Pair;
 import bayonet.distributions.ExhaustiveDebugRandom;
 import bayonet.math.NumericalUtils;
 import blang.engines.internals.PosteriorInferenceEngine;
+import blang.inits.Arg;
+import blang.inits.DefaultValue;
 import blang.inits.GlobalArg;
 import blang.inits.experiments.ExperimentResults;
 import blang.io.BlangTidySerializer;
@@ -16,14 +18,14 @@ public class Exact implements PosteriorInferenceEngine
 {
   @GlobalArg ExperimentResults results;
   
-  SampledModel model;
-
-  @Override
-  public void setSampledModel(SampledModel model) 
-  {
-    this.model = model;
-  }
-
+  @Arg(description = "Stop and warn the user if more than this number of random draws is used in a simulation.")
+         @DefaultValue("20")
+  public int maxDepth = 20;
+  
+  @Arg(description = "Stop and warn the user if more than this number of traces is used in a simulation.")
+          @DefaultValue("2_000_000")
+  public int maxTraces = 2_000_000;
+  
   @SuppressWarnings("unchecked")
   @Override
   public void performInference() 
@@ -34,13 +36,33 @@ public class Exact implements PosteriorInferenceEngine
     while (exhaustive.hasNext())
     {
       model.forwardSample(exhaustive, false);
+      
+      // check this is not going to take forever
+      if (exhaustive.lastDepth() > maxDepth || i > maxTraces)
+        throw new RuntimeException("The number of traces or trace depth has been exceeded. "
+            + "You should probably switch to an approximate inference engine. "
+            + "You can also increase these limits via command line arguments if you are sure. "
+            + "currentDepth=" + exhaustive.lastDepth() + " nTraces=" + i);
+      
       double logWeightFromModel = model.logDensity(1.0);
+      
+      // checks agreement of generate and law blocks
       double logWeightFromGeneration = Math.log(exhaustive.lastProbability()) + model.logDensity(1.0) - model.logDensity(0.0);
       if (!NumericalUtils.isClose(logWeightFromModel, logWeightFromGeneration, NumericalUtils.THRESHOLD))
-        throw new RuntimeException("Generate(rand){..} block not faithful with its laws{..} block.");
+        throw new RuntimeException("generate(rand){..} block not faithful with its laws{..} block.");
+      
       model.getSampleWriter(tidySerializer).write(Pair.of("sample", i++), Pair.of("logWeight", logWeightFromModel)); 
     }
   }
+  
+  SampledModel model;
+
+  @Override
+  public void setSampledModel(SampledModel model) 
+  {
+    this.model = model;
+  }
+
 
   @Override
   public void check(GraphAnalysis analysis) 
