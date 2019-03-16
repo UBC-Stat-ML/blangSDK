41b6fcb4bd2951b10b9b1d119df2093a4c8ca789
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2019-03-16 01:27:41 -0700

    First shot at partition update mechanism

--- src/main/java/blang/engines/internals/EngineStaticUtils.java
+++ src/main/java/blang/engines/internals/EngineStaticUtils.java
@@ -1,6 +1,17 @@
 package blang.engines.internals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.math3.analysis.UnivariateFunction;
+import org.apache.commons.math3.analysis.solvers.PegasusSolver;
+
+import com.google.common.collect.Lists;
+import com.google.common.primitives.Doubles;
+
 import bayonet.distributions.Multinomial;
+import bayonet.math.NumericalUtils;
 import bayonet.smc.ParticlePopulation;
 import blang.runtime.SampledModel;
 
@@ -48,4 +59,44 @@ public class EngineStaticUtils
     return 2.0 * sum / N / N;
   }
   
+  /**
+   * 
+   * @param annealingParameters length N + 1
+   * @param acceptanceProbabilities length N, entry i is accept b/w chain i-1 and i
+   * @param nGrids number of grids in output partition (including both end points)
+   * @return list of size nGrids with optimized partition, sorted in increasing order
+   */
+  public static List<Double> optimalPartition(List<Double> annealingParameters, List<Double> acceptanceProbabilities, int nGrids) 
+  {
+    if (annealingParameters.size() != acceptanceProbabilities.size() + 1)
+      throw new RuntimeException();
+    for (double pr : acceptanceProbabilities)
+      if (!(pr >= 0.0 && pr <= 1.0))
+         throw new RuntimeException();
+    
+    double [] xs = Doubles.toArray(annealingParameters);
+    double [] ys = new double[xs.length];
+    for (int i = 1; i < ys.length; i++)
+      ys[i] = ys[i-1] + (1.0 - acceptanceProbabilities.get(i-1));
+    double Lambda = ys[ys.length - 1];
+    
+    Spline spline = Spline.createMonotoneCubicSpline(xs, ys);
+    
+    List<Double> result = new ArrayList<>();
+    double previous = 0.0;
+    PegasusSolver solver = new PegasusSolver();
+    for (int i = 0; i < nGrids; i++) 
+    {
+      double y = Lambda * i / (nGrids - 1.0);
+      double point = solver.solve(10_000, (double x) -> spline.interpolate(x) - y, previous, 1.0);
+      result.add(point);
+    }
+    return result;
+  }
+  
+  public static void main(String [] args) 
+  {
+    System.out.println(optimalPartition(Arrays.asList(0.0, 0.5, 1.0), Arrays.asList(0.1, 0.9), 5));
+  }
+
 }
