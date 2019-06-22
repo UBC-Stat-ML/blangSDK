73b4cb6c68f186ced7050e537c0e21eacf4c15fc
Kevin Chern <kevinn.chern@gmail.com>
2019-06-22 11:32:06 -0700

    tidy up setup.sh

--- setup.sh
+++ setup.sh
@@ -9,18 +9,18 @@ mkdir -p build/xtend/test
 mkdir -p build/blang/test
 
 
+
+
 # Setup Eclipse
 ./gradlew eclipse
 
 # Fix some stuff that get broken everytime
 git -c diff.mnemonicprefix=false -c core.quotepath=false -c credential.helper=sourcetree checkout -- .settings/ca.ubc.stat.blang.BlangDsl.prefs .settings/org.eclipse.jdt.core.prefs .settings/org.eclipse.xtend.core.Xtend.prefs .settings/org.eclipse.xtext.java.Java.prefs
-mkdir build/blang/main
-mkdir build/xtend/main
+
 
 
 
 # Completion messages.
-
 echo "Eclipse setup complete."
 
 if hash blang 2>/dev/null; then
