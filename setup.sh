143ae7a77cfa4f74d2431ebd902c34098a65ccf7
Kevin Chern <kevinn.chern@gmail.com>
2019-06-26 06:58:04 -0700

    combine scripts

--- setup.sh
+++ setup.sh
@@ -44,7 +44,5 @@ echo
 # Fix some stuff that get broken everytime
 git -c diff.mnemonicprefix=false -c core.quotepath=false -c credential.helper=sourcetree checkout -- .settings/ca.ubc.stat.blang.BlangDsl.prefs .settings/org.eclipse.jdt.core.prefs .settings/org.eclipse.xtend.core.Xtend.prefs .settings/org.eclipse.xtext.java.Java.prefs
 
-mkdir -p build/blang/test
-mkdir    build/blang/main
+mkdir -p build/blang/main
 mkdir -p build/xtend/main
-mkdir    build/xtend/test
