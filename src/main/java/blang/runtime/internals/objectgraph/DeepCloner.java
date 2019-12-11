475780b6ff152a82df2582f75e049904afb2cb1c
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2019-12-10 16:32:30 -0800

    Expose deep cloner to allow configurating it

--- src/main/java/blang/runtime/internals/objectgraph/DeepCloner.java
+++ src/main/java/blang/runtime/internals/objectgraph/DeepCloner.java
@@ -4,7 +4,7 @@ import com.rits.cloning.Cloner;
 
 public class DeepCloner 
 {
-  private static final Cloner cloner = new Cloner(); // thread safe
+  public static final Cloner cloner = new Cloner(); // thread safe
     // cloner.nullTransient = true // not a good idea, Java SDK uses transient liberally e.g the data array in ArrayList!
     // override registerFastCloners() {} // not needed after all, but may want to add LinkedHashSet at some point
   
