a5894e2726f1b1e59383354c3b98928fd9a99a19
Kevin Chern <kevinn.chern@gmail.com>
2021-03-17 12:16:44 -0700

    check bash or zsh for setting up cli

--- setup-cli.sh
+++ setup-cli.sh
@@ -25,12 +25,19 @@ echo
 if hash blang 2>/dev/null; then
     echo 
 else
-    echo "NOTE: We are adding a line into ~/.bash_profile to make the blang CLI command"
+    echo "NOTE: We are adding a line into ~/.bash_profile or ~/.zshenv to make the blang CLI command"
     echo "      accessible from any directory (as blang is not found in PATH right now)."
     echo
     to_add="$(pwd)/build/install/blang/bin/"
     existing='$PATH'
     line="export PATH=${existing}:${to_add}"
     export PATH=$PATH:${to_add}
-    echo $line >>~/.bash_profile
+    if [[ $(basename $SHELL) == "zsh" ]]; then
+        echo $line >>~/.zshenv
+    elif [[ $(basename $SHELL) == "bash" ]]; then
+        echo $line >>~/.bash_profile
+    else
+        echo "Default shell is not Bash nor Zsh."
+        echo "Please add $(to_add) to PATH manually."
+    fi
 fi
