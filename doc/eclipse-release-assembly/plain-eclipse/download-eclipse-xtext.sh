d1bbb5f3a51794e7c8a63f44bfbdecd22fc85a11
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2021-03-17 22:24:15 -0700

    Kevin's fix to setup-eclipse + updated eclipse release for BlangIDE build

--- doc/eclipse-release-assembly/plain-eclipse/download-eclipse-xtext.sh
+++ doc/eclipse-release-assembly/plain-eclipse/download-eclipse-xtext.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget http://downloads.yoctoproject.org/eclipse-full/technology/epp/downloads/release/2019-03/R/eclipse-dsl-2019-03-R-macosx-cocoa-x86_64.dmg
+wget https://ftp.osuosl.org/pub/eclipse/technology/epp/downloads/release/2020-12/R/eclipse-dsl-2020-12-R-macosx-cocoa-x86_64.dmg
 
 echo "Unpack and put Eclipse.app in here"
--- setup-eclipse.sh
+++ setup-eclipse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./gradlew eclipse
+./gradlew assemble eclipse
 
 # Fix some stuff that get broken everytime
 git -c diff.mnemonicprefix=false -c core.quotepath=false -c credential.helper=sourcetree checkout -- .settings/ca.ubc.stat.blang.BlangDsl.prefs .settings/org.eclipse.jdt.core.prefs .settings/org.eclipse.xtend.core.Xtend.prefs .settings/org.eclipse.xtext.java.Java.prefs
