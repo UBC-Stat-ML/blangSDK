878c4c0456bfd7a8c771eb5e0de38ee1eba793a8
Kevin Chern <kevinn.chern@gmail.com>
2021-07-19 14:11:30 -0700

    revert to c98874dd49

--- src/main/java/blang/engines/internals/schedules/TemperatureSchedule.java
+++ src/main/java/blang/engines/internals/schedules/TemperatureSchedule.java
@@ -4,7 +4,7 @@ import bayonet.smc.ParticlePopulation;
 import blang.inits.Implementations;
 import blang.runtime.SampledModel;
 
-@Implementations({AdaptiveTemperatureSchedule.class, FixedTemperatureSchedule.class})
+@Implementations({AdaptiveTemperatureSchedule.class, FixedTemperatureSchedule.class}) 
 public interface TemperatureSchedule
 {
   double nextTemperature(ParticlePopulation<SampledModel> population, double annealingParam, double maxAnnealParam);
